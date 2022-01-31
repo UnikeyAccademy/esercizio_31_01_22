b1c6939bc6f3485787631acdfd348c5c20f35707
Edoardo Carradori <edoardo.carradori.96@gmail.com>
2022-01-31 15:56:29 +0100

    Creata classe Distributore

---
+++ src/it/unikey/Distributore.java
@@ -0,0 +1,12 @@
+package it.unikey;
+
+public class Distributore {
+
+    private double deposito;
+    private double euroPerLitro;
+
+    public Distributore(double euroPerLitro) {
+        this.deposito = 0;
+        this.euroPerLitro = euroPerLitro;
+    }
+}
